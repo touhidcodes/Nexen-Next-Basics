bd2fe26576593b40dfa5afb732b67f53ab90643a
touhidcodes <touhidcodes@gmail.com>
2025-09-08 23:41:28 +0600

    add: navbar

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
-const Navbar = () => {
-  return (
-    <div>
-      <h2>Welcome to the Navbar page</h2>
-    </div>
-  );
-};
+import Link from "next/link";
 
-export default Navbar;
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-500 text-white p-4 flex gap-6">
+      <Link href="/">Home</Link>
+      <Link href="/todos">Todos (Static)</Link>
+      <Link href="/server-todos">Todos (SSR)</Link>
+      <Link href="/client-todos">Todos (Client)</Link>
+    </nav>
+  );
+}
